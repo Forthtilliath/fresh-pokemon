caa900965c47e1851ab2560dbee56b86158542d1
Forth <33458877+Forthtilliath@users.noreply.github.com>
2022-07-21 14:18:24 +0200

    github

--- utils/env.ts
+++ utils/env.ts
@@ -1,5 +1,5 @@
-import { config } from "https://deno.land/x/dotenv@v1.0.1/mod.ts";
-config({ export: true });
+// import { config } from "https://deno.land/x/dotenv@v1.0.1/mod.ts";
+// config({ export: true });
 
 export const DB = Deno.env.get("DB");
 export const TOKEN = Deno.env.get("TOKEN");
