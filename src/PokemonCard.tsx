12972ae52d62b8edf3b08e5417d3b933264cac08
Forth <33458877+Forthtilliath@users.noreply.github.com>
2022-07-21 05:08:24 +0200

    First commit

---
+++ src/PokemonCard.tsx
@@ -0,0 +1,25 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw } from "@twind";
+
+import { PokemonCardProps } from "../utils/types.ts";
+// import AddToList from "../islands/AddToList.tsx";
+
+export default function PokemonCard({ pokemon, allowAdd }: PokemonCardProps) {
+  return (
+    <div class={tw`rounded-xl border-1 p-5`}>
+      <div class={tw`text-2xl font-bold mb-2`}>{pokemon.name}</div>
+      <img src={pokemon.image} />
+      <div class={tw`mt-5 flex`}>
+        <a class={tw`underline flex-grow`} href={`/pokemon/${pokemon.id}`}>
+          View Details...
+        </a>
+        {allowAdd && (
+          <div class={tw`flex-end`}>
+            {/* <AddToList pokemon={pokemon} /> */}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ utils/types.ts
@@ -0,0 +1,26 @@
+export type Pokemon = {
+  id: string;
+  name: string;
+  type: string[];
+  hp: string;
+  attack: string;
+  defense: string;
+  special_attack: string;
+  special_defense: string;
+  speed: string;
+  image: string;
+};
+
+export type HomeProps = {
+  pokemons: Pokemon[];
+  query: string;
+};
+
+export type PokemonCardProps = {
+  pokemon: Pokemon;
+  allowAdd?: boolean;
+};
+
+export type DetailsProps = {
+  pokemon: Pokemon;
+};
